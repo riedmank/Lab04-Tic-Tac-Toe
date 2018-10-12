bf51729b5c44d8fba8228149d81744f7e46bac30
Kristofer Riedman <riedmank@hotmail.com>
2018-10-12 11:53:12 -0700

    added 6 more tests; all tests pass

--- TicTacToeTest/UnitTest1.cs
+++ TicTacToeTest/UnitTest1.cs
@@ -13,6 +13,14 @@ namespace TicTacToeTest
             Player p1 = new Player("test 1", "X");
             Player p2 = new Player("test 2", "O");
             Game Game = new Game(p1, p2);
+            Board Board = new Board();
+            Board.GameBoard = new string[,]
+            {
+                {"X","X","X" },
+                {"4", "5", "6" },
+                {"7", "8", "9" }
+            };
+            Game.Board = Board;
             Assert.True(Game.CheckForWinner(Game.Board));
         }
 
@@ -22,6 +30,14 @@ namespace TicTacToeTest
             Player p1 = new Player("test 1", "X");
             Player p2 = new Player("test 2", "O");
             Game Game = new Game(p1, p2);
+            Board Board = new Board();
+            Board.GameBoard = new string[,]
+            {
+                {"X","2","3" },
+                {"X", "5", "6" },
+                {"X", "8", "9" }
+            };
+            Game.Board = Board;
             Assert.True(Game.CheckForWinner(Game.Board));
         }
 
@@ -31,6 +47,14 @@ namespace TicTacToeTest
             Player p1 = new Player("test 1", "X");
             Player p2 = new Player("test 2", "O");
             Game Game = new Game(p1, p2);
+            Board Board = new Board();
+            Board.GameBoard = new string[,]
+            {
+                {"X","2","3" },
+                {"4", "X", "6" },
+                {"7", "8", "X" }
+            };
+            Game.Board = Board;
             Assert.True(Game.CheckForWinner(Game.Board));
         }
 
@@ -43,6 +67,47 @@ namespace TicTacToeTest
             p1.IsTurn = true;
             Game.SwitchPlayer();
             Assert.True(p2.IsTurn);
+            Assert.False(p1.IsTurn);
+        }
+
+        [Fact]
+        public void UserInputCorrelatesToCorrectBoardSpaceRow()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Position position = new Position(1, 1);
+            Position position1 = Player.PositionForNumber(5);
+            Assert.Equal(position.Row, position1.Row);
+        }
+
+        [Fact]
+        public void UserInputCorrelatesToCorrectBoardSpaceColumn()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Position position = new Position(1, 1);
+            Position position1 = Player.PositionForNumber(5);
+            Assert.Equal(position.Column, position1.Column);
+        }
+
+        [Fact]
+        public void TieGameTest()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Board Board = new Board();
+            Board.GameBoard = new string[,]
+            {
+                {"X","O","X" },
+                {"O", "X", "X" },
+                {"O", "X", "O" }
+            };
+            Game.Board = Board;
+            Game.CheckForWinner(Game.Board);
+            Assert.Null(Game.Winner);
         }
     }
 }
