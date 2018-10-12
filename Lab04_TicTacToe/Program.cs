f3c9f7f90d216cc823561cb82183ccd7c75ff638
Kristofer Riedman <riedmank@hotmail.com>
2018-10-12 07:47:35 -0700

    initial commit

---
+++ Lab04_TicTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab04_TicTacToe
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public static void PlayGame()
+		{
+			// TODO: Instantiate your players
+			// Create the Game
+			// Play the Game
+			// Output the winner
+		}
+	}
+}
---
+++ TicTacToeTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Lab04_TicTacToe.Program;
+
+namespace TicTacToeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
