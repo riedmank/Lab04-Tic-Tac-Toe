899fbda6a3f39a824b7786e6e8b831696791723c
Kristofer Riedman <riedmank@hotmail.com>
2018-10-12 11:11:32 -0700

    added switch player test; test passes

--- TicTacToeTest/UnitTest1.cs
+++ TicTacToeTest/UnitTest1.cs
@@ -1,15 +1,48 @@
 using System;
 using Xunit;
 using static Lab04_TicTacToe.Program;
+using Lab04_TicTacToe.Classes;
 
 namespace TicTacToeTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CheckForHorizontalWin()
         {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Assert.True(Game.CheckForWinner(Game.Board));
+        }
 
+        [Fact]
+        public void CheckForVerticalWin()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Assert.True(Game.CheckForWinner(Game.Board));
+        }
+
+        [Fact]
+        public void CheckForDiagonalWin()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            Assert.True(Game.CheckForWinner(Game.Board));
+        }
+
+        [Fact]
+        public void CheckTurnSwitch()
+        {
+            Player p1 = new Player("test 1", "X");
+            Player p2 = new Player("test 2", "O");
+            Game Game = new Game(p1, p2);
+            p1.IsTurn = true;
+            Game.SwitchPlayer();
+            Assert.True(p2.IsTurn);
         }
     }
 }
